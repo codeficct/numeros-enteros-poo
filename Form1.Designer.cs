45bf97d1295bba9148dbc73e6df674b214fb8bd6
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-08 09:19:46 -0400

    feat: add method when user click on menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,9 @@
             this.checkIfIsFibonacciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.getFactorialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.isOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,18 +54,18 @@
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(178, 175);
+            this.textBox1.Location = new System.Drawing.Point(105, 130);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.Size = new System.Drawing.Size(258, 25);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(178, 257);
+            this.textBox2.Location = new System.Drawing.Point(105, 274);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.Size = new System.Drawing.Size(258, 25);
             this.textBox2.TabIndex = 1;
             // 
             // button1
@@ -70,7 +73,7 @@
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(308, 172);
+            this.button1.Location = new System.Drawing.Point(369, 127);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 28);
             this.button1.TabIndex = 2;
@@ -83,7 +86,7 @@
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(308, 254);
+            this.button2.Location = new System.Drawing.Point(369, 271);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 28);
             this.button2.TabIndex = 3;
@@ -107,7 +110,7 @@
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(485, 410);
+            this.button3.Location = new System.Drawing.Point(474, 367);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(95, 28);
             this.button3.TabIndex = 5;
@@ -145,12 +148,14 @@
             this.isParToolStripMenuItem.Name = "isParToolStripMenuItem";
             this.isParToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.isParToolStripMenuItem.Text = "isPar";
+            this.isParToolStripMenuItem.Click += new System.EventHandler(this.isParToolStripMenuItem_Click);
             // 
             // isMultipleToolStripMenuItem
             // 
             this.isMultipleToolStripMenuItem.Name = "isMultipleToolStripMenuItem";
             this.isMultipleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.isMultipleToolStripMenuItem.Text = "isMultiple";
+            this.isMultipleToolStripMenuItem.Click += new System.EventHandler(this.isMultipleToolStripMenuItem_Click);
             // 
             // isSubMultipleToolStripMenuItem
             // 
@@ -188,12 +193,44 @@
             this.isOrderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.isOrderToolStripMenuItem.Text = "isOrder";
             // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(105, 194);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(84, 25);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "First Param";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(101, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Result";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(592, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -231,6 +268,9 @@
         private System.Windows.Forms.ToolStripMenuItem checkIfIsFibonacciToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem getFactorialToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem isOrderToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
