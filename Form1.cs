cc2ce56d1356faf0ef55aeaef168420296379055
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-07 22:25:14 -0400

    feat: create first object using the class IntegerNumber

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,45 @@ namespace NumeroEntero_poo
 {
     public partial class Form1 : Form
     {
+        IntegerNumber objNumber1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objNumber1 = new IntegerNumber();
+        }
+
+        // Message Error
+        public void errorHandler(string message, string title)
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text.Length == 0) throw new ArgumentException("Por favor ingrese un dato.");
+                objNumber1.setNumber(int.Parse(textBox1.Text));
+            }
+            catch (Exception error)
+            {
+                errorHandler(error.Message, "Datos incorrectos o faltantes.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = objNumber1.getNumber().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
         }
     }
 }
