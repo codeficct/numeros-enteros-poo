3b20cb9cc6cd70330bbe7bcbd01c7620e013ce66
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-07 22:24:27 -0400

    style: add buttons, menu and labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isParToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isMultipleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isSubMultipleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isPrimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isPalindromeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkIfIsFibonacciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getFactorialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.isOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(178, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(178, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(308, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Set number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(308, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Get number";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Practice of Integer Numbers";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(485, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 28);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(592, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.isParToolStripMenuItem,
+            this.isMultipleToolStripMenuItem,
+            this.isSubMultipleToolStripMenuItem,
+            this.isPrimeToolStripMenuItem,
+            this.isPalindromeToolStripMenuItem,
+            this.checkIfIsFibonacciToolStripMenuItem,
+            this.getFactorialToolStripMenuItem,
+            this.isOrderToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // isParToolStripMenuItem
+            // 
+            this.isParToolStripMenuItem.Name = "isParToolStripMenuItem";
+            this.isParToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isParToolStripMenuItem.Text = "isPar";
+            // 
+            // isMultipleToolStripMenuItem
+            // 
+            this.isMultipleToolStripMenuItem.Name = "isMultipleToolStripMenuItem";
+            this.isMultipleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isMultipleToolStripMenuItem.Text = "isMultiple";
+            // 
+            // isSubMultipleToolStripMenuItem
+            // 
+            this.isSubMultipleToolStripMenuItem.Name = "isSubMultipleToolStripMenuItem";
+            this.isSubMultipleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isSubMultipleToolStripMenuItem.Text = "isSubMultiple";
+            // 
+            // isPrimeToolStripMenuItem
+            // 
+            this.isPrimeToolStripMenuItem.Name = "isPrimeToolStripMenuItem";
+            this.isPrimeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isPrimeToolStripMenuItem.Text = "isPrime";
+            // 
+            // isPalindromeToolStripMenuItem
+            // 
+            this.isPalindromeToolStripMenuItem.Name = "isPalindromeToolStripMenuItem";
+            this.isPalindromeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isPalindromeToolStripMenuItem.Text = "isPalindrome";
+            // 
+            // checkIfIsFibonacciToolStripMenuItem
+            // 
+            this.checkIfIsFibonacciToolStripMenuItem.Name = "checkIfIsFibonacciToolStripMenuItem";
+            this.checkIfIsFibonacciToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkIfIsFibonacciToolStripMenuItem.Text = "checkIfIsFibonacci";
+            // 
+            // getFactorialToolStripMenuItem
+            // 
+            this.getFactorialToolStripMenuItem.Name = "getFactorialToolStripMenuItem";
+            this.getFactorialToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.getFactorialToolStripMenuItem.Text = "getFactorial";
+            // 
+            // isOrderToolStripMenuItem
+            // 
+            this.isOrderToolStripMenuItem.Name = "isOrderToolStripMenuItem";
+            this.isOrderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.isOrderToolStripMenuItem.Text = "isOrder";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(592, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(608, 489);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isParToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isMultipleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isSubMultipleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isPrimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isPalindromeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkIfIsFibonacciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getFactorialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem isOrderToolStripMenuItem;
     }
 }
 
