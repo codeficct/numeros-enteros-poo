e2f9817a43f588d87c0cd24ae5365280624a12a6
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-11 14:21:28 -0400

    refactor: remove comments

--- Vector.cs
+++ Vector.cs
@@ -135,20 +135,20 @@ namespace NumeroEntero_poo
             }
         }
 
-        public void SelectInOrder(ref Vector vectorResult)
-        {
-            int index;
-            IntegerNumber objIntNumber = new IntegerNumber();
-            vectorResult.number = 0;
-            for (index = 1; index <= number; index++)
-            {
-                objIntNumber.setNumber(numbers[index]);
-                if (objIntNumber.isOrder())
-                {
-                    vectorResult.number++;
-                    vectorResult.numbers[vectorResult.number] = numbers[index];
-                }
-            }
-        }
+        //public void SelectInOrder(ref Vector vectorResult)
+        //{
+        //    int index;
+        //    IntegerNumber objIntNumber = new IntegerNumber();
+        //    vectorResult.number = 0;
+        //    for (index = 1; index <= number; index++)
+        //    {
+        //        objIntNumber.setNumber(numbers[index]);
+        //        if (objIntNumber.isOrder())
+        //        {
+        //            vectorResult.number++;
+        //            vectorResult.numbers[vectorResult.number] = numbers[index];
+        //        }
+        //    }
+        //}
     }
 }
